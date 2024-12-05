e3e7eb23cb2967a488d8643bcc56419a22d2d01a
bth <>
2024-12-05 15:04:27 -0500

    Changed distutils to setuputils since distutils is now deprecated in Python 3.12

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
+
+import numpy
 
 DEBUG = True
 
@@ -8,6 +10,7 @@ skinmodule = Extension(
     'skin',
     sources=['skinmodule.c', 'skintalk.c', 'profile.c', 'layout.c'],
     libraries=['pthread'],
+    include_dirs=[numpy.get_include()],
     extra_compile_args=['-DDEBUG'] if DEBUG else [],
     language='c11',
 )
